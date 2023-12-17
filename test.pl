75a1fa20b70b47560b90773afac9e6845e847027
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2023-12-17 11:06:41 +0100

    Add primitive manual fuzz testing

--- test.pl
+++ test.pl
@@ -2,6 +2,8 @@
 
 use strict;
 
+use File::stat;
+
 assert_produces_correct_output('in.tex', 'correct.txt');
 assert_produces_correct_output('in.tex', 'correct.txt', '-l');
 assert_produces_correct_output('noinclude.tex', 'noinclude-correct.txt', '-n');
@@ -19,6 +21,8 @@ run_for_wrong_input("test/unterminated-verb-pipe.tex");
 
 run_for_input_with_error("test/unterminated-verb.tex");
 run_for_input_with_error("test/unterminated-verb-eol.tex");
+
+fuzz();
 
 print "Tests ok\n";
 
@@ -58,6 +62,22 @@ sub run_for_input_with_error {
 	# https://www.perlmonks.org/?node_id=81640
 	my $res = system(get_cmd("./delatex $input")) >> 8;
 	die "exit code $res" if ($res != 1);
+}
+
+sub fuzz {
+	chdir 'test';
+	my $input = 'in.tex';
+	my $sb = stat($input);
+	for (my $i=0; $i<25; ++$i) {
+		my $bytes = int(rand($sb->size));
+		printf("Fuzz: bytes %5d/%5d of %s. ", $bytes, $sb->size, $input);
+		system("head -c $bytes $input > /tmp/trunc.tex");
+		my $res = system(get_cmd("../delatex /tmp/trunc.tex > /tmp/trunc.log")) >> 8;
+		print "Exit code $res\n";
+		if ($res != 0 && $res != 1) {
+			die;
+		}
+	}
 }
 
 sub execute_cmd {
